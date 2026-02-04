b34496cf6062333dc582e21a419ee44c3ba7a544
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-04 19:52:19 +0600

    66136

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from random import choice
 # Create your models here.
 
 class OrderStatus(models.Model):
@@ -8,14 +7,15 @@ class OrderStatus(models.Model):
         ('PENDING','pending'),
         ('PROCESSING','processing'),
         ('COMPLETED','completed'),
-        ('CANCELLED','cancelled'),
+        ('CANCELED','canceled'),
     ]
     name = models.CharField(
         max_length=50,
-        unique=True, 
-        choices=statuses,
+        unique=True,
+        choice=statuses,
         default='PENDING',
-        verbose_name="StatusName")
+        verbose_name="StatusName"
+    )
     # string represent
     def __str__(self):
         return self.name
