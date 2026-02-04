5bcac6aa25fa25f2748351614d4b8e76b2af45b1
zishan344 <zishanahmed344@gmail.com>
2026-02-04 19:28:07 +0600

    setup vscode on locally

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
 # Create your models here.
+# Menue category mdoel for food menu
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="CategoryName")
+    # string represent
+    def __str__(self):
+        return self.name
