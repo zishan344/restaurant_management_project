9b61c9355bc888730c2a1153dbf92181d6dad3a0
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-09 12:04:32 +0600

    Update models.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+# Create your models here.
+class Restaurant(models.Model):
+    # write your code here
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name="RestaurantName"
+    )
+    owner_name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name="Name"
+    )
+    email = models.EmailField(
+        max_length=250,
+        null=False,
+        blank=False,
+        verbose_name="Email"
+    )
+    phone_number = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+        verbose_name="ContactNumber"
+    )
+    address = models.TextField(
+        null=False,
+        blank=False,
+        verbose_name="Address"
+    )
+    city = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name="City"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="created time."
+    )
+
+    class Meta:
+        # default ordering
+        ordering = ['-created_at']
