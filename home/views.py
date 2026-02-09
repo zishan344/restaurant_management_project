012c4fc112dd9fcc0ebc4db31bfba497f5d8cd3f
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-09 20:05:29 +0600

    66293

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from rest_framework import generics
 from home.serializers import MenuCategorySerializer
 from home.models import MenuCategory
+from products.models import MenuItem
+from products.serializers import MenuItemSerializer
 # Create your views here.
 
 class MenuCategoryView(generics.ListAPIView):
     queryset = MenuCategory.objects.all()
     serializer_class = MenuCategorySerializer
+
+class FeaturedMenuView(generics.ListAPIView):
+    queryset = MenuItem.objects.filter(is_featured=True)
+    serializer_class = MenuItemSerializer
