7a68926530936539bb1c9a970a1dc28cf29e886a
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-09 20:23:19 +0600

    66290

--- account/models.py
+++ account/models.py
@@ -1,52 +1,31 @@
 from django.db import models
 
 # Create your models here.
-
-from django.db import models
-
-# Create your models here.
 class Restaurant(models.Model):
-    # write your code here
     name = models.CharField(
         max_length=100,
-        null=False,
-        blank=False,
         verbose_name="RestaurantName"
     )
-    owner_name = models.CharField(
-        max_length=100,
-        null=False,
-        blank=False,
-        verbose_name="Name"
-    )
-    email = models.EmailField(
+    owner_name = models.EmailField(
         max_length=250,
-        null=False,
-        blank=False,
         verbose_name="Email"
     )
-    phone_number = models.CharField(
+    ponoe_number = models.CharField(
         max_length=50,
-        null=False,
-        blank=False,
         verbose_name="ContactNumber"
     )
     address = models.TextField(
-        null=False,
-        blank=False,
         verbose_name="Address"
     )
     city = models.CharField(
         max_length=100,
-        null=False,
-        blank=False,
         verbose_name="City"
     )
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name="created time."
+    has_delivery = models.BooleanField(
+        default=True,
+        verbose_name="delivery service availability"
     )
-
-    class Meta:
-        # default ordering
-        ordering = ['-created_at']
+    created_at = models.DateTimeField(
+        auto_now_add=True
+        verbose_name="created time"
+    )
