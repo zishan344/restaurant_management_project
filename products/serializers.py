095c296a67bbd8ef2868e6c41c059f5bdfb9ad1f
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-09 19:59:46 +0600

    66293

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from .models import Item
+from .models import MenuItem
 
-class ItemSerializer(serializers.ModelSerializer):
+class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
