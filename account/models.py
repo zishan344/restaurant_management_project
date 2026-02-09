7f33dcf8d19885df8e89c9137b489bf953f91cd0
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-09 20:59:32 +0600

    66290

--- account/models.py
+++ account/models.py
@@ -4,32 +4,41 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(
         max_length=100,
-        verbose_name="Restaurant Name")
+        verbose_name="Restaurant Name"
+        )
     owner_name = models.CharField(
         max_length=100,
-        verbose_name="Owner Name")
+        verbose_name="Owner Name"
+        )
     email = models.EmailField(
         max_length=150,
-        verbose_name="Email")
+        verbose_name="Email"
+        )
 
     phone_number = models.CharField(
         max_length=50,
-        verbose_name="Contact Number")
+        verbose_name="Contact Number"
+        )
     address = models.TextField(
-        verbose_name="Address")
+        verbose_name="Address"
+        )
     city = models.CharField(
         max_length=100,
-        verbose_name="City")
+        verbose_name="City"
+        )
     has_delivery = models.BooleanField(
         default=True,
-        verbose_name="Has Delivery")
+        verbose_name="Has Delivery"
+        )
     created_at = models.DateTimeField(
         auto_now_add=True,
-        verbose_name="Created At")
+        verbose_name="Created At"
+        )
 
     class Meta:
         ordering = ["-created_at"]
-        verbose_name= "Restaurant"
-        verbose_name_plural= "Restaurant"
+        verbose_name = "Restaurant"
+        verbose_name_plural = "Restaurants"
+
     def __str__(self):
         return self.name
