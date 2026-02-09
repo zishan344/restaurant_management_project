9d9277181aac0bb49fd273332f6c64f44e24df09
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-09 20:06:50 +0600

    66293

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("menu_category/", MenuCategoryView.as_view(), name="menu-category")
+    path("menu_category/", MenuCategoryView.as_view(), name="menu-category"),
+    path("featured_item/", FeaturedMenuView.as_view(), name="featured-item")
 ]
