415ffe8ee1f4af6cb0c7d4328eac9fe34237e962
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-09 19:59:15 +0600

    66293

--- products/models.py
+++ products/models.py
@@ -1,10 +1,36 @@
 from django.db import models
-
+from account.models import Restaurant
 # Create your models here.
-class Item(models.Model):
-    item_name = models.CharField(max_length=150)
-    item_price = models.DecimalField(max_digits=10, decimal_places=2)
+class MenuItem(models.Model):
+    ''' restaurant menu item '''
+    restaurant = models.ForeignKey(
+        Restaurant,
+        on_delete=models.CASCADE,
+        related_name="restaurant"
+        verbose_name="restaurant"
+    )
+    name = models.CharField(
+        max_length=150,
+        verbose_name="item_name"
+        )
+    description = models.TextField(
+        null = True, 
+
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="price"
+        ),
+    is_available = models.BooleanField(
+        default = True,
+        verbose_name="available_item"
+    ),
+    is_featured = models.BooleanField(
+        default=False,
+        verbose_name="featured_item"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.item_name)
+        return str(self.name)
