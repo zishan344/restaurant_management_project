9fcfb967d0de6a9f7fcbc192866f98efb63a6243
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-09 20:36:06 +0600

    66290

--- account/models.py
+++ account/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(
         max_length=100,
-        verbose_name="RestaurantName"
+        verbose_name="Restaurant_Name"
     )
     owner_name = models.EmailField(
         max_length=250,
@@ -12,7 +12,7 @@ class Restaurant(models.Model):
     )
     ponoe_number = models.CharField(
         max_length=50,
-        verbose_name="ContactNumber"
+        verbose_name="Contact_Number"
     )
     address = models.TextField(
         verbose_name="Address"
@@ -23,9 +23,12 @@ class Restaurant(models.Model):
     )
     has_delivery = models.BooleanField(
         default=True,
-        verbose_name="delivery service availability"
+        verbose_name="delivery_service_availability"
     )
     created_at = models.DateTimeField(
-        auto_now_add=True
-        verbose_name="created time"
+        auto_now_add=True,
+        verbose_name="created_time"
     )
+
+    class Meta:
+        ordering = ["-created_at"]
