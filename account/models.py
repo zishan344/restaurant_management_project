41c9f2ba4dcd524d324ee20f4f029d8ed8d2f159
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-10 20:41:08 +0600

    66293

--- account/models.py
+++ account/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Restaurant(models.Model):
+    ''' Restaurant Model '''
     name = models.CharField(
         max_length=100,
         verbose_name="Restaurant Name"
